e09cfc32d4a6032f8e3af1926120cecd13534c49
David Kral <dockral@gmail.com>
2025-04-23 21:07:43 -0700

    Prod fix, 500 on first login attempt

--- backend/config/database.py
+++ backend/config/database.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("❌ DATABASE_URL not found.")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
